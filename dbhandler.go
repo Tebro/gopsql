32304306b0bec418808679bf946ec396c4366e50
Richard Weber <riche.weber@gmail.com>
2017-03-29 15:06:31 +0300

    Moved from stringbuilding to prepared statements

--- dbhandler.go
+++ dbhandler.go
@@ -46,7 +46,7 @@ func getSelectAllQuery(obj interface{}) (string, bool) {
 		if _, ok := objType.Field(i).Tag.Lookup("column_skip"); !ok {
 			fieldNames = append(fieldNames, objType.Field(i).Name)
 		}
-		if _, ok := objType.Field(i).Tag.Lookup("column_order_by"); !ok {
+		if _, ok := objType.Field(i).Tag.Lookup("column_order_by"); ok {
 			orderBy = true
 			orderByField = objType.Field(i).Name
 		}
@@ -76,28 +76,34 @@ func getSelectFilteredQuery(obj interface{}, filterString string) string {
 		retval = fmt.Sprintf("%s WHERE %s;", q, filterString)
 	}
 
-	logger.Debug(fmt.Sprintf("Built filtered SELECT query: %s", retval))
+	logger.Debug(fmt.Sprintf("Built filtered SELECT query: '%s'", retval))
 
 	return retval
 }
 
-func parseFilterString(filters ...string) (string, error) {
+func parseFilterString(filters ...string) (string, []string, error) {
 	if len(filters) < 2 {
-		return "", errors.New("Not enough parameters provided")
+		return "", nil, errors.New("Not enough parameters provided")
 	}
+	var filterValues []string
+
 	if len(filters) == 2 {
-		return fmt.Sprintf("%s='%s'", filters[0], filters[1]), nil
+		filterValues = append(filterValues, filters[1])
+		return fmt.Sprintf("%s=$1", filters[0]), filterValues, nil
 	}
 	var parts []string
 	if (len(filters)+1)%3 == 0 {
+		valueIndex := 1
 		for i := 0; i < len(filters); i += 3 {
-			parts = append(parts, fmt.Sprintf("%s='%s'", filters[i], filters[i+1]))
+			filterValues = append(filterValues, filters[i+1])
+			parts = append(parts, fmt.Sprintf("%s=$%d", filters[i], valueIndex))
+			valueIndex++
 			if i+2 < len(filters) {
 				parts = append(parts, filters[i+2])
 			}
 		}
 	}
-	return strings.Join(parts, " "), nil
+	return strings.Join(parts, " "), filterValues, nil
 }
 
 // GetAll gets all the rows for the type
@@ -109,13 +115,18 @@ func GetAll(obj interface{}) (*sql.Rows, error) {
 
 // GetFiltered does a query with a WHERE section GetFiltered(sometype, field, value, "AND", field, value)
 func GetFiltered(obj interface{}, filter ...string) (*sql.Rows, error) {
-	parsedFilter, err := parseFilterString(filter...)
+	parsedFilter, filterValues, err := parseFilterString(filter...)
 	if err != nil {
 		return nil, err
 	}
 	q := getSelectFilteredQuery(obj, parsedFilter)
 
-	return db.Query(q)
+	filterValueInterfaces := make([]interface{}, len(filterValues))
+	for i, v := range filterValues {
+		filterValueInterfaces[i] = v
+	}
+
+	return db.Query(q, filterValueInterfaces...)
 }
 
 // Saveable interface is so that Save function always has a way of getting and setting the ID
@@ -126,28 +137,40 @@ type Saveable interface {
 
 // UpdateExisting updates a record in the DB based on ID
 func UpdateExisting(obj Saveable) error {
-	q := getUpdateQuery(obj)
-	_, err := db.Exec(q)
+	q, values := getUpdateQuery(obj)
+
+	valueInterfaces := make([]interface{}, len(values))
+	for i, v := range values {
+		valueInterfaces[i] = v
+	}
+
+	_, err := db.Exec(q, valueInterfaces...)
 	return err
 }
 
 // InsertNew inserts a new entry and returns the Row for someone with more knowledge to retrieve ID
 func InsertNew(obj Saveable) *sql.Row {
-	q := getInsertQuery(obj)
+	q, values := getInsertQuery(obj)
 
-	return db.QueryRow(q)
+	valueInterfaces := make([]interface{}, len(values))
+	for i, v := range values {
+		valueInterfaces[i] = v
+	}
 
+	logger.Debug(fmt.Sprintf("Going to insert with: '%v' and %v", q, valueInterfaces))
+
+	return db.QueryRow(q, valueInterfaces...)
 }
 
 const updateQueryTemplate = "UPDATE %s SET %s WHERE %s;"
 
-func getUpdateQuery(obj Saveable) string {
+func getUpdateQuery(obj Saveable) (string, []string) {
 	objVal := reflect.ValueOf(obj)
 	columnName := objVal.Type().Name()
 
-	filter := fmt.Sprintf("ID=%d", obj.GetID())
-
 	var updateValues []string
+	var values []string
+	valueIndex := 1
 	for i := 0; i < objVal.NumField(); i++ {
 		field := objVal.Type().Field(i)
 		_, found1 := field.Tag.Lookup("column_skip")
@@ -155,29 +178,32 @@ func getUpdateQuery(obj Saveable) string {
 		if !found1 && !found2 {
 			vInter := objVal.Field(i).Interface()
 			val := reflect.ValueOf(vInter)
-			var theValue string
-			if val.Kind() == reflect.String {
-				theValue = fmt.Sprintf("'%v'", val) // Extra quotes for strings
-			} else {
-				theValue = fmt.Sprintf("%v", val)
-			}
-			updateValues = append(updateValues, fmt.Sprintf("%s=%v", field.Name, theValue))
+
+			theValue := fmt.Sprintf("%v", val)
+
+			updateValues = append(updateValues, fmt.Sprintf("%s=$%d", field.Name, valueIndex))
+			valueIndex++
+			values = append(values, theValue)
 		}
 	}
+	filter := fmt.Sprintf("ID=$%d", valueIndex)
+	values = append(values, fmt.Sprintf("%v", obj.GetID())) //Add the ID for the WHERE part
 
-	return fmt.Sprintf(updateQueryTemplate, columnName, strings.Join(updateValues, ","), filter)
+	return fmt.Sprintf(updateQueryTemplate, columnName, strings.Join(updateValues, ","), filter), values
 
 }
 
 const insertQueryTemplate = "INSERT INTO %s (%s) VALUES (%s) RETURNING ID;"
 
-func getInsertQuery(obj Saveable) string {
+func getInsertQuery(obj Saveable) (string, []string) {
 	objVal := reflect.ValueOf(obj)
 	columnName := objVal.Type().Name()
 
 	// Build Fields and values list
 	var fields []string
 	var values []string
+	var valuePlaceholders []string
+	valueIndex := 1
 	for i := 0; i < objVal.NumField(); i++ {
 		field := objVal.Type().Field(i)
 		_, found1 := field.Tag.Lookup("column_skip")
@@ -188,31 +214,27 @@ func getInsertQuery(obj Saveable) string {
 			// Getting the actual value
 			vInter := objVal.Field(i).Interface()
 			val := reflect.ValueOf(vInter)
-			var theValue string
-			if val.Kind() == reflect.String {
-				theValue = fmt.Sprintf("'%v'", val) // Extra quotes for strings
-			} else {
-				theValue = fmt.Sprintf("%v", val)
-			}
+			theValue := fmt.Sprintf("%v", val)
 
 			values = append(values, theValue)
+			valuePlaceholders = append(valuePlaceholders, fmt.Sprintf("$%d", valueIndex))
+			valueIndex++
 		}
 	}
 
-	return fmt.Sprintf(insertQueryTemplate, columnName, strings.Join(fields, ","), strings.Join(values, ","))
+	return fmt.Sprintf(insertQueryTemplate, columnName, strings.Join(fields, ", "), strings.Join(valuePlaceholders, ", ")), values
 
 }
 
-const deleteQueryTemplate = "DELETE FROM %s WHERE ID='%v';"
-
-func getDeleteQuery(obj Saveable) string {
-	columnName := reflect.ValueOf(obj).Type().Name()
-	return fmt.Sprintf(deleteQueryTemplate, columnName, obj.GetID())
-}
+const deleteQueryTemplate = "DELETE FROM %s WHERE ID=$1;"
 
 // Delete removes the provided entry from the database, based on the return value of GetID()
 func Delete(obj Saveable) error {
-	_, err := db.Exec(getDeleteQuery(obj))
+
+	columnName := reflect.ValueOf(obj).Type().Name()
+	query := fmt.Sprintf(deleteQueryTemplate, columnName)
+
+	_, err := db.Exec(query, obj.GetID())
 	return err
 }
 
